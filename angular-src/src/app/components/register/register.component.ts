4cc802190eb5d27444116d6c6e4eb752e08317f8
Adam Tse <mr.adam.tse@gmail.com>
2017-02-19 04:57:28 -0500

    form event

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+name: String;
+username: String;
+email: String;
+password: String;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRegisterSubmit(){
+    console.log('SUBMITED')
+    console.log(this.name)
+    console.log(this.username)
   }
 
 }
